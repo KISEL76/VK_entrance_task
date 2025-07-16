c655b64a6fd9464d165a2c6807aab7638b40a1b6
Vladimir <72561517+KISEL76@users.noreply.github.com>
2025-07-16 12:49:49 +0300

    start_decomposition_done

---
+++ db/init.sql
@@ -0,0 +1,16 @@
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  login VARCHAR(32) NOT NULL UNIQUE CHECK (char_length(login) >= 3),
+  password_hash TEXT NOT NULL,
+  created_at TIMESTAMP DEFAULT NOW()
+);
+
+CREATE TABLE ads (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(100) NOT NULL CHECK (char_length(title) >= 5),
+  description VARCHAR(500),
+  image_url TEXT,
+  price NUMERIC NOT NULL CHECK (price >= 0),
+  author_id INTEGER NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+  created_at TIMESTAMP DEFAULT NOW()
+);
